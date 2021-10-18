0f31299fcf4aa7cf33d8f77396005c98f1c283c0
BertrandToupance <bertoupance@gmail.com>
2021-10-18 12:08:06 +0200

    Update Heros.java

--- Heros.java
+++ Heros.java
@@ -1,5 +1,10 @@
 public class Heros extends Entite{
 
-	
+	// fonction estMort si en contacte avec un monstre
+	// fonction combat
+	// fonction déplacement
 	
 	public Heros(String nom, String attaque)
+	
+	
+	
