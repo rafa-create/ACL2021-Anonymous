c6592aba47e257fa2599bc072a6d658514ba1437
BertrandToupance <bertoupance@gmail.com>
2021-10-18 16:17:37 +0200

    Update Heros.java

--- Heros.java
+++ Heros.java
@@ -3,8 +3,21 @@ public class Heros extends Entite{
 	// fonction estMort si en contacte avec un monstre
 	// fonction combat
 	// fonction déplacement
+	//les héros ont une certaine jauge de vie
 	
-	public Heros(String nom, String attaque)
+	private int PdV
 	
 	
+	public Heros(String nom, int attaque, int placement_x, int placement_y, int PdV) {
+		super(nom, attaque, placement_x, placement_y);
+		
 	
+	
+
+	public boolean EstMort() {
+		
+		
+		
+		
+	}
+	}
