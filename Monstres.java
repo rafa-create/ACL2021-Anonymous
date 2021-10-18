730c6326acf8d8296ced5158ab27ab60b3e6f923
BertrandToupance <bertoupance@gmail.com>
2021-10-18 12:08:02 +0200

    Update Monstres.java

--- Monstres.java
+++ Monstres.java
@@ -1,1 +1,11 @@
-
+public class Monstres extends Entite{
+	
+	
+	// fonction déplacement (aléatoire)
+	// fonction déplamcent proche joueur
+	// t
+	
+	public Monstres (String name, String ) {
+	
+	
+}
