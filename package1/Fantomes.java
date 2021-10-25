23742680b98e8ca00f986565f50a442d1d520c29
rafa-create <rafaelorset@hotmail.fr>
2021-10-25 17:42:56 +0200

    Create Fantomes.java

---
+++ package1/Fantomes.java
@@ -0,0 +1,16 @@
+package package1;
+public class Fantomes extends Entite{
+	
+	
+	// déplacement intelligents sur toute la carte (focus joueur)
+	// traverse les murs
+	// 1 seul pv
+	
+	public Fantomes(String nom, ){
+		
+		
+		
+		
+		
+	}
+}
