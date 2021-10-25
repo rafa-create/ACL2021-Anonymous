71a46564fb89a409695fdb84bb2d195164b8f157
rafa-create <rafaelorset@hotmail.fr>
2021-10-25 17:43:01 +0200

    Create Heros.java

---
+++ package1/Heros.java
@@ -0,0 +1,24 @@
+package package1;
+public class Heros extends Entite{
+
+	// fonction estMort si en contacte avec un monstre
+	// fonction combat
+	// fonction déplacement
+	//les héros ont une certaine jauge de vie
+	
+	private int PdV
+	
+	
+	public Heros(String nom, int attaque, int placement_x, int placement_y, int PdV) {
+		super(nom, attaque, placement_x, placement_y);
+		
+	
+	
+
+	public boolean EstMort() {
+		
+		
+		
+		
+	}
+	}
