403ac8e917d83067f7fb60f02ef1d353eef947e8
rafa-create <rafaelorset@hotmail.fr>
2021-11-04 10:57:15 +0100

    Create Jedi.java

---
+++ src/jeu/Jedi.java
@@ -0,0 +1,19 @@
+package Jeu;
+
+import javax.swing.JFrame;
+
+public class Jedi extends JFrame{
+
+	public Jedi() 
+    {
+		add(new Niveaux());
+	}
+	public static void main(String[] args) {
+		Jedi Obi = new Jedi();
+		Obi.setVisible(true);
+		Obi.setTitle("Star wars");
+		Obi.setSize(380,420);
+		Obi.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		Obi.setLocationRelativeTo(null);
+	}
+}
