f2d54545635d8ed9671890cab85273d22bb0dc61
rafa-create <rafaelorset@hotmail.fr>
2021-11-22 11:44:30 +0100

    Create Pion.java

---
+++ Jeu/src/jeu/Pion.java
@@ -0,0 +1,31 @@
+package jeu;
+
+public class Pion {
+	int X;
+	int Y;
+	int Vie;
+	
+	public Pion(int X, int Y,int Vie) {
+		this.X=X;
+		this.Y=Y;
+		this.Vie=Vie;
+	}
+	public int getX() {
+		return(this.X);
+	}
+	public int getY() {
+		return(this.Y);
+	}
+	public void setX(int x) {
+		this.X=this.X+x*23;
+	}
+	public void setY(int y) {
+		this.Y=this.Y+y*23;
+	}
+	public int getV() {
+		return(this.Vie);
+	}
+	public void setV(int v) {
+		this.Vie=this.Vie+v;
+	}
+}
