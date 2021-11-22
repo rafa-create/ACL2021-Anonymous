d934fa5c9a6c6d273894a0d34833c7ae477a9b17
rafa-create <rafaelorset@hotmail.fr>
2021-11-22 23:20:25 +0100

    Update Niveaux.java

--- src/jeu/Niveaux.java
+++ src/jeu/Niveaux.java
@@ -5,8 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.util.Random;
-
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.Timer;
