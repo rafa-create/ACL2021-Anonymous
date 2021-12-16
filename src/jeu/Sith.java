3ed0989ce5c813ca1e0f5bf820ea367e85bcd2e5
rafa-create <rafaelorset@hotmail.fr>
2021-12-17 00:30:15 +0100

    Update Sith.java

--- src/jeu/Sith.java
+++ src/jeu/Sith.java
@@ -16,13 +16,19 @@ public class Sith
 	public int getY1() {
 		return(this.Y);
 	}
-	public void setX1() {
-		this.X=this.X+genererInt(-1,1)*23;
+	public void setX1(int x ) {
+		this.X=this.X+x*23;
 	}
-	public void setY1() {
-		this.Y=this.Y+genererInt(-1,1)*23;
+	public void setY1(int y ) {
+		this.Y=this.Y+y*23;
 	}
 	public int genererInt(int borneInf, int borneSup){
+		   Random random = new Random();
+		   int nb;
+		   nb = borneInf+random.nextInt(borneSup-borneInf);
+		   return nb;
+		}
+	public int genererInt2(int borneInf, int borneSup){
 		   Random random = new Random();
 		   int nb;
 		   nb = borneInf+random.nextInt(borneSup-borneInf);
