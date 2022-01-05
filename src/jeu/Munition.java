ba5d20255560b4dab9b4c6df7dd7b52e70ffe6c0
rafa-create <rafaelorset@hotmail.fr>
2022-01-05 16:33:05 +0100

    Create Munition.java

---
+++ src/jeu/Munition.java
@@ -0,0 +1,16 @@
+package jeu;
+
+public class Munition {
+	int x;
+	int y;
+	public Munition(int x , int y ) {
+		this.x=x;
+		this.y=y;
+	}
+	public int GetX2() {
+		return this.x;
+	}
+	public int GetY2() {
+		return this.y;
+	}
+}
